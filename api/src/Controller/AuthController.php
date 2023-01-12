63cd8c42c316bb65b474342bcbe7de83dc6ae9d4
Piotr Jarema <piotr.jarema@lppsa.com>
2023-01-12 14:27:04 +0100

    Login endpoint

--- api/src/Controller/AuthController.php
+++ api/src/Controller/AuthController.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\ApiToken;
 use App\Entity\User;
+use App\Repository\ApiTokenRepository;
+use App\Repository\UserRepository;
 use DateTimeImmutable;
 use Doctrine\Persistence\ManagerRegistry;
 use Exception;
@@ -13,13 +16,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class AuthController extends AbstractController
 {
     /**
      * @throws Exception
      */
-    #[Route('/api/register', name: 'app_auth', methods: ['POST'])]
+    #[Route('/api/auth/register', name: 'app_auth_register', methods: ['POST'])]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, ManagerRegistry $doctrine): JsonResponse
     {
         // TODO: 
@@ -38,8 +42,7 @@ class AuthController extends AbstractController
         $user->setStreetNumber($request->get('street_number'));
         $user->setCity($request->get('city'));
         $user->setPostalCode($request->get('postal_code'));
-        
-        $user->setRoles('ROLE_USER');
+
         $entityManager->persist($user);
         $entityManager->flush();
         
@@ -49,11 +52,81 @@ class AuthController extends AbstractController
         ]);
     }
 
+    #[Route('/api/auth/login', name: 'app_auth_login', methods: ['POST'])]
+    public function login(Request $request,
+                          UserRepository $userRepository, 
+                          UserPasswordHasherInterface $userPasswordHasher,
+                          ManagerRegistry $doctrine,
+                          ApiTokenRepository $apiTokenRepository
+    ): JsonResponse
+    {
+        $userData = [
+            'email' => $request->get('email'),
+            'password' => $request->get('password')
+        ];
+        
+        // Get user
+        $user = $userRepository->findOneBy(['email' => $userData['email']]);
+        
+        // Check if user exists
+        if($user === null) {
+            $message = sprintf("User with email %s does not exist!", $userData['email']);
+            
+            return $this->json([
+                'message' => $message,
+                'code' => Response::HTTP_NOT_FOUND
+            ]);
+        }
+        
+        // Check if password is valid
+        if(!$userPasswordHasher->isPasswordValid($user, $userData['password'])) {
+            $message = sprintf("Password for email %s is invalid!", $userData['email']);
+
+            return $this->json([
+                'message' => $message,
+                'code' => Response::HTTP_UNAUTHORIZED
+            ]);
+        }
+        
+        $entityManager = $doctrine->getManager();
+        
+        // Remove old token if exists
+        $oldToken = $apiTokenRepository->findOneBy(['user' => $user]);
+        if($oldToken !== null) {
+            $entityManager->remove($oldToken);
+            $entityManager->flush();
+        }
+        
+        // Generate token
+        $token = $this->generateToken($user);
+
+        // Add token to db
+        $entityManager->persist($token);
+        $entityManager->flush();
+
+        return $this->json([
+            'token' => $token->getToken(),
+            'code' => Response::HTTP_OK
+        ]);
+    }
+
     /**
      * @throws Exception
      */
     protected function prepareDateOfBirth(string $date): DateTimeImmutable
     {
         return new DateTimeImmutable($date);
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function generateToken(User $user)
+    {
+        $token = new ApiToken();
+        $token->setUser($user);
+        $token->setToken(base64_encode($user->getEmail() . bin2hex(random_bytes(20))));
+        
+        return $token;
     }
 }
