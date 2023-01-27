a642942798770846921c7f4f8d15005bdafc4fa8
Piotr Jarema <kontakt.zevstudio@gmail.com>
2023-01-27 16:35:19 +0100

    Header

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,17 +1,19 @@
 import LoginForm from "@/components/LoginForm"
 import RegisterForm from "@/components/RegisterForm"
 import IndexLayout from "@/layouts/IndexLayout";
+import styles from "@/styles/components/Index.module.scss"
+import Link from 'next/link'
 
 export default function Home() {
     return (
         <IndexLayout>
-            <RegisterForm />
-            <RegisterForm />
-            <RegisterForm />
-            <RegisterForm />
-            <RegisterForm />
-            <RegisterForm />
-            <RegisterForm />
+            <main className={styles.main}>
+                <div className={styles.welcome}>
+                    <Link href="/"><a>FOR MAN</a></Link>
+                    <Link href="/"><a>FOR WOMAN</a></Link>
+                    <Link href="/"><a>FOR KID</a></Link>
+                </div>
+            </main>
         </IndexLayout>
     )
 }
